19076d5132d6ffbc8cea6b4b00c15b7122b1e380
Matthias Eisenhut <matthias.eisenhut.2003@gmail.com>
2024-05-07 17:50:17 +0200

    getting Animators from server

--- client/src/views/AnimatorView.vue
+++ client/src/views/AnimatorView.vue
@@ -1,5 +1,98 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useAnimatorStore } from '@/stores/animators.js';
+
+const animatorStore = useAnimatorStore();
+const columns = [
+  { name: 'image', label: 'Image', align: 'center', field: 'images' },
+  { name: 'content', label: 'Content', align: 'left', field: 'content' },
+  { name: 'actions', label: 'Actions', align: 'center', field: 'actions' },
+];
+const pagination = ref({
+  sortBy: 'date_of_birth',
+  descending: true,
+  page: 1,
+  rowsPerPage: 5,
+});
+
+const animators = ref([]);
+const openAnimator = ref(false);
+const clickedAnimator = ref(null);
+
+onMounted(async () => {
+  await animatorStore.fetchAnimators();
+  animators.value = animatorStore.data;
+  console.log(animators.value);
+});
+
+const calcAge = (dob) => {
+  const diff = Date.now() - new Date(dob).getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+const strToDt = (str) => {
+  return new Date(str).toLocaleDateString('sq-AL');
+};
+
+const openAnimatorDialog = (evt, row) => {
+  openAnimator.value = true;
+  clickedAnimator.value = row;
+  console.log(row);
+};
+</script>
+
 <template>
   <q-page padding>
-    <span>Animators</span>
+    <q-table
+      :columns="columns"
+      :rows="animators.data"
+      hide-header
+      v-model:pagination="pagination"
+      table-style="width: 75vw"
+      @row-click="openAnimatorDialog"
+    >
+      <template #body-cell-image="props">
+        <td>
+          <q-img
+            :src="`http://localhost:3000/images/animators/${props.row.image}`"
+            width="10vw"
+          ></q-img>
+        </td>
+      </template>
+      <template #body-cell-content="props">
+        <td>
+          <p class="text-h4">Name: {{ props.row.name }}</p>
+          <p class="text-h4">Age: {{ calcAge(props.row.date_of_birth) }}</p>
+          <p class="text-h4">Birthday: {{ strToDt(props.row.date_of_birth) }}</p>
+        </td>
+      </template>
+    </q-table>
+    <q-dialog v-model="openAnimator" full-height
+      ><q-card class="my-card">
+        <q-card-section class="row items-center">
+          <div class="text-h6"></div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <img :src="`http://localhost:3000/images/animators/${clickedAnimator.image}`" />
+
+        <q-card-section>
+          <div class="text-h6">{{ clickedAnimator.name }}</div>
+          <div class="text-subtitle2">Age: {{ calcAge(clickedAnimator.date_of_birth) }}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ clickedAnimator.about_me }}
+        </q-card-section>
+      </q-card></q-dialog
+    >
   </q-page>
 </template>
+
+<style scoped>
+.my-card {
+  width: 75vw;
+}
+</style>

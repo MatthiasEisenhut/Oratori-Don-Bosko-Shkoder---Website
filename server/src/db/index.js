d9239d80a4ba369dc0dbc250e54f8e3bd062eb7f
Matthias Eisenhut <matthias.eisenhut.2003@gmail.com>
2024-05-22 10:19:43 +0200

    fixed ssl-error (this time ffor real)

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 
 pg.types.setTypeParser(1082, (value) => value);
 
-const pool = new pg.Pool({ sslmode: 'require' });
+const pool = new pg.Pool({ ssl: true });
 
 const query = (text, params) => pool.query(text, params);
 
